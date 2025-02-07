7ed96a1cc16483eddcf625956fe9d1425d795abd
Tanya301 <tanya@samokhvalov.com>
2025-02-06 17:30:13 -0800

    pie chart and legend

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,4 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
@@ -7,7 +8,37 @@ const projectsContainer = document.querySelector('.projects');
 const titleElement = document.querySelector('.projects-title'); // Select the title element
 
 if (titleElement) {
-  titleElement.textContent = `${projects.length} Projects`;
+    titleElement.textContent = `${projects.length} Projects`;
 }
 
 renderProjects(projects, projectsContainer, 'h2');
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+];
+let sliceGenerator = d3.pie().value((d) => d.value);
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors(idx))
+})
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`) // Define the CSS variable on <li>
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`)
+          .attr('class', 'legend');
+});
